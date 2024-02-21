a13de1037150637f4eb31a8bb771e25ec4aa4331
marlboroMan10 <142051923+marlboroMan10@users.noreply.github.com>
2024-02-21 21:24:19 +0530

    first commit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\hp\\Downloads\\flutter_windows_3.10.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\hp\\OneDrive\\Desktop\\Flutter Project\\music_player_app-master" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\hp\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\hp\\OneDrive\\Desktop\\Flutter Project\\music_player_app-master"
+  "FLUTTER_ROOT=C:\\Users\\hp\\Downloads\\flutter_windows_3.10.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\hp\\OneDrive\\Desktop\\Flutter Project\\music_player_app-master\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\hp\\OneDrive\\Desktop\\Flutter Project\\music_player_app-master"
+  "FLUTTER_TARGET=C:\\Users\\hp\\OneDrive\\Desktop\\Flutter Project\\music_player_app-master\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\hp\\OneDrive\\Desktop\\Flutter Project\\music_player_app-master\\.dart_tool\\package_config.json"
+)
